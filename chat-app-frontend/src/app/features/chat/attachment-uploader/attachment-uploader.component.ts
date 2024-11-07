5771cc217a30ba4358f8a870e931d00a7c0a903a
VarenytsiaV <victoriavarenytsia@gmail.com>
2024-11-07 19:57:09 +0200

    initial commit

---
+++ chat-app-frontend/src/app/features/chat/attachment-uploader/attachment-uploader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attachment-uploader',
+  templateUrl: './attachment-uploader.component.html',
+  styleUrls: ['./attachment-uploader.component.scss']
+})
+export class AttachmentUploaderComponent {
+
+}
